0d68503e87c86623c5af80b372767e13af1da01e
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-03 17:18:14 -0500

    Comentarios

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -10,3 +10,10 @@ var welcomeMessage : String
 welcomeMessage = "Hola, ¿que tal?"
 
 var red, green, blue : Double
+
+print("El numero actual es: \(currentLoginAttempt) de un total disponible de: \(maximumNumberOfLoginAttemps)")
+
+// Esto  es un comentario
+
+/* Que se ponen los girasoles para salir?
+ Unos plantalones (risas.mp3).*/
