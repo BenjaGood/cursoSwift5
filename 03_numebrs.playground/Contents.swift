b56540d9a2726826f25cc816a2fb30f12e476f31
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-03 17:51:05 -0500

    Archivo numbers added

---
+++ 03_numebrs.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+let age : UInt8 = 19
+
+let minValue = Int64.min
+let maxValue = Int64.max
+
+let f1 : Float = 3.14159265
+let f2: Double = 3.14159265
+
+let meaningOfLife = 42 //Int
+let pi = 3.14159 //Double
+let anotherPi = 3 + 0.14159 //Double
+
+let demicalInteger = 17
+let binatyInteger = 0b10001
+let octalInteger = 0o21
+let hexadecimalInteger = 0x11
+
+let decimalDouble = 12.1875
+let exponentDounle = 1.21875e1
+let hexadecimalBouble1 = 0xC.3p0
+
+let paddedDouble = 123.456
+let someNumber = 000000097.54
+
+let oneMillon = 1_000_000
+let justMoreThanAMillion = 1_000_000.000_000_1
+
+let cannotBeNegative: UInt8 = 0
+let tooBig: UInt8 = 200
