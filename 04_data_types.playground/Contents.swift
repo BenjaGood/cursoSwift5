9013bd34370dcc526c84966c4f13c233903190dc
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-04 15:37:15 -0500

    Booleanos

---
+++ 04_data_types.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let orangesAreOragne = true
+let foodIsDelicious = false
+
+var isAged : Bool
+
+isAged = true
+
+if isAged {//Solo entramos aquí, si isAged == true
+    print("Puedes entrar")
+}else{//Solo entramos aquí, si isAged == false
+    print("No puedes pasar")
+}
+
+var age = 18
+if age >= 18 {
+    print("Puedes entrar")
+}
+else{
+    print("No puedes entrar")
+}
