a4e3416d1309f33ccec0d2276d0248e7adf0b0ce
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-04 15:49:18 -0500

    Tuplas

--- 04_data_types.playground/Contents.swift
+++ 04_data_types.playground/Contents.swift
@@ -20,3 +20,18 @@ if age >= 18 {
 else{
     print("No puedes entrar")
 }
+
+
+//TUPLAS
+let http404Error = (404, "Pagina no encontrada")
+let (statusCode, statusMessage) = http404Error
+print("El codigo del estado es \(statusCode)")
+print("El mansaje del servior es \(statusMessage)")
+
+let (justStatusCode, _) = http404Error
+print("El codigo del estado es \(justStatusCode)")
+
+print("El codigo del error es \(http404Error.0) y el mesnaje es  \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print("El codigo del estado es \(http200Status.statusCode) y el mensaje es \(http200Status.description)")
