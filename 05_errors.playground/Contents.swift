9a8f443987e0ca53380f5c473e94372ca8ea451c
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-05 15:51:06 -0500

    Finalice el ultimo archivo

--- 05_errors.playground/Contents.swift
+++ 05_errors.playground/Contents.swift
@@ -1,3 +1,56 @@
 import UIKit
 
-var str = "Hello, playground"
+func canThrowError() throws{
+    //Aqui hay codigo que puede causar el error
+}
+
+do{
+    try canThrowError()
+    //si llegamos aqui, no hay error
+}catch{
+    //si se llega aqui hay un error
+}
+
+
+func makeASandwitch() throws{
+    
+}
+
+do{
+    try makeASandwitch()
+    //Me como el sandwitch
+}catch {
+    //tengo platos limpios -> lavar platos
+    //tengo ingredientes -> ir a comprar
+    //tengo hambre -> esperar un tiempo
+}
+
+
+func cleanPlate() throws{
+    
+}
+
+do{
+    try cleanPlate()
+    //Procede a la accion hacer el sandwich
+}
+catch {
+    //comprar platos
+}
+
+//Aserciones (debug) y Precondiciones (build)
+
+let age = 19
+
+//.... aquí el codigo sigue
+if age > 10 {
+    print("puedes subir a la montaña rusa")
+}
+else if age >= 0{
+    print("Eres demasiado pequeño")
+}
+else {
+    assertionFailure("La edad de una perosna no es negativa")
+}
+
+
