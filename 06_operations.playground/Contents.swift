4652361b4e5c18ff502e3efe7deec57f1311c541
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-07 19:05:49 -0500

    Agregue los utimos cambios

--- 06_operations.playground/Contents.swift
+++ 06_operations.playground/Contents.swift
@@ -3,3 +3,64 @@ import UIKit
 let b = 10
 var a = 5
 a = b
+
+let (x,y) = (1,2)
+
+if a == b {
+    print("Los valores de a y b son iguales")
+}
+
+1+2
+5-3
+2*3
+10.0/2.5
+
+"Hello " + "World"
+
+9/4 //division
+9%4 //residuo
+
+9 == 4*2+1
+
+let five = 5
+let minusFive = -five
+let plusFive = -minusFive
+
+let minusSix = -6
+let alsoMinusSix = -minusSix
+
+var number = 5
+number += 3 // number = number + 3
+number -= 2 // number = number - 2
+number *= 5 // number = number * 5
+number /= 6 // number = number / 6
+number %= 2 // number = number % 2
+
+//DECIMALES
+9.0/4.0
+
+1 == 1
+1 == 2
+1 != 2
+2 > 1
+2 < 1
+2 >= 1
+1 >= 1
+2 <= 1
+
+let name = "Ricardo Celis"
+
+if name == "Juan Gabriel"{
+    print("Bienvenido \(name), eres invitado a la fiesta")
+}
+else{
+    print("Lo siento \(name), no puedes pasar...")
+}
+
+(1, "Juan Gabriel") < (2, "Ricardo Celis")
+
+(3, "Juan Gabriel") < (3, "Ricardo Celis") //La nomenclatura es de abeceradio, agarra J y R y por orden alfabetico.
+(3, "Ricardo Celis") < (3, "Juan Gabriel")
+
+(18, "Benjamin Gutierrez") <= (31, "Juan Gomilla")
+(18, "Benjamin Gutierrez") >= (31, "Juan Gomilla")
