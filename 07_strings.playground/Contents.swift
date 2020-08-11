dfb62da2facd29cad8d2935acb5dc83e1dc0ff14
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-11 17:31:55 -0500

    Nuevos cambios al ultimo arhchivo

--- 07_strings.playground/Contents.swift
+++ 07_strings.playground/Contents.swift
@@ -24,3 +24,52 @@ let text = """
  Soy de Mexico \(flagMex)\
  y estoy tomando el curso de Swift 5 en platzi!
 """
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print("El string esta vacio")
+}else{
+    print("El string tiene valor")
+}
+
+var newSomeString = "Un caballo"
+newSomeString += " y un carruaje"
+newSomeString += " y un soldado"
+
+let aString = "Juan Gabriel"
+//aString += " y Ricardo Celis" <-- ERROR, PORQUE ES UNA LET (CONSTANTE)
+
+var a = "A"
+var b = "B"
+print("a vale \(a), b vale \(b)")
+b = a
+print("a vale \(a), b vale \(b)")
+b = "C"
+print("a vale \(a), b vale \(b)")
+a = "D"
+print("a vale \(a), b vale \(b)")
+
+let myName = "Benjamin"
+for ch in name {
+    print(ch)
+}
+print(name.count)
+
+let exclamationMark : Character = "!"
+
+let nameChars: [Character] =  ["B", "e", "n", "j", "a", "m", "i", "n"]
+var nameString = String(nameChars)
+nameString.append(flagMex)
+print("\(nameString)")
+
+let compoundName = "Ben" + "jamin" + "."
+
+nameString.append(exclamationMark)
+//APEND JUNTA STRINGS
+
+let multiplier = 3
+var message = "El producto de \(multiplier) x 3.5 da \(Double(multiplier)*3.5)"
+message.append(exclamationMark)
+
+
