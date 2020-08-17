eb153e17bd7dcc1b469a6f48e0c0439667806cec
Benjamin Gutierrez <ben77gome@gmail.com>
2020-08-17 16:12:03 -0500

    Agregue el if y while

--- 09_control_flow.playground/Contents.swift
+++ 09_control_flow.playground/Contents.swift
@@ -30,3 +30,57 @@ let minuteInterval = 5
 for tickMark in stride(from: 0, to: minutes, by: minuteInterval){
     print("\(hour): \(tickMark)")
 }
+//EN UN FOR, EL TICKMARK O EL IDX, SE QUEDA SOMO EN EL BUCLE, YA QUE SOLAMENTE LO AGREGAN LOS CORCHETES...
+
+//Bucle While
+
+var i = 0
+while i<=10 {
+    i += 1
+}
+print(i)
+
+repeat{
+    i+=1
+} while i<=10
+//EL REPEAT FUERZAN EL CONTEO
+print(i)
+
+let endCount = 100
+var start = 2
+var prime: [Int] = []
+while start <= 100{
+var isPrime = true
+for idx in 2..<start {
+    if start % idx == 0{
+        if idx != i && idx != start{
+            isPrime = false
+        }
+    }
+}
+    if isPrime {
+        prime.append(start)
+    }
+    start += 1
+}
+
+// if y else
+var temperature = 30
+if temperature <= 15{
+    print("Hace frio!, enciende la calefaccion")
+}else if temperature >= 21{
+    print("Mucho calor, falta el aire aconidiconado")
+}else{
+    print("Todo bien, el clima esta perfecto")
+}
+
+var money = 501
+if money <= 200 {
+    print("Perfecto!, te doy el dinero 😉")
+}else if money <= 300{
+    print("Ok, ten el dinero")
+}else if money <= 500{
+    print("Es mucho, ¿no?, pero te los presto")
+}else{
+    print("No tengo dinero, lo siento")
+}
