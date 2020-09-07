fc647e9a94bb0743c12c55b46ac6fb51e3595898
Benjamin Gutierrez <ben77gome@gmail.com>
2020-09-07 15:50:10 -0500

    Nuevo Tema

---
+++ 13_struct_class.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+//Struct vs Class
+
+struct SomeStruct{
+    //Definición de la estructura aquí
+}
+class SomeClass{
+    //La definicion de la clase aquí
+}
+
+struct Resolution{
+    var width = 0
+    var height = 0
+}
+
+class videoMode{
+    var resolution = Resolution()
+    var interlaced = false
+    var framerate = 0.0
+    var name: String?
+}
+
+let someResolution = Resolution()
+let someVideoMode = videoMode()
+
+print(someResolution.width)
+someVideoMode.resolution.width = 1280
+print(someVideoMode.resolution.width)
+
+someVideoMode.framerate = 60
+print(someVideoMode.framerate)
+
+let vga = Resolution(width: 640, height: 480)
+vga.width
+vga.height
+
+let hd = Resolution(width: 1920, height: 1080)
+
+var cinema = hd
+print("\(cinema.width) x \(cinema.height)")
+cinema.width = 2048
+print("\(cinema.width) x \(cinema.height)")
+print("\(hd.width) x \(hd.height)")
+
+enum CompassPoint{
+    case north, south, east, west
+}
+
+var currentDirection = CompassPoint.north
+let oldDirection = currentDirection
+currentDirection = .south
+
+print(currentDirection)
+print(oldDirection)
